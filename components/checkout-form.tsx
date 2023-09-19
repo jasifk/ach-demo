e037785d03791d94368a66e35818386fb609a858
Jasif Shameem K <jasifk@newagesmb.com>
2023-09-19 15:10:36 +0530

    apple pay

--- components/checkout-form.tsx
+++ components/checkout-form.tsx
@@ -2,8 +2,10 @@ import {
   PaymentElement,
   useStripe,
   useElements,
+  PaymentRequestButtonElement,
 } from "@stripe/react-stripe-js";
-import React from "react";
+import { PaymentRequest } from "@stripe/stripe-js";
+import React, { useEffect, useState } from "react";
 
 export default function CheckoutForm({
   clientSecret,
@@ -12,6 +14,71 @@ export default function CheckoutForm({
 }) {
   const stripe = useStripe();
   const elements = useElements();
+
+  const [paymentRequest, setPaymentRequest] = useState<PaymentRequest | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (paymentRequest && stripe) {
+      paymentRequest.on("paymentmethod", async (ev) => {
+        // Confirm the PaymentIntent without handling potential next actions (yet).
+        const { paymentIntent, error: confirmError } =
+          await stripe.confirmCardPayment(
+            clientSecret,
+            { payment_method: ev.paymentMethod.id },
+            { handleActions: false }
+          );
+
+        if (confirmError) {
+          // Report to the browser that the payment failed, prompting it to
+          // re-show the payment interface, or show an error message and close
+          // the payment interface.
+          ev.complete("fail");
+        } else {
+          // Report to the browser that the confirmation was successful, prompting
+          // it to close the browser payment method collection interface.
+          ev.complete("success");
+          // Check if the PaymentIntent requires any actions and, if so, let Stripe.js
+          // handle the flow. If using an API version older than "2019-02-11"
+          // instead check for: `paymentIntent.status === "requires_source_action"`.
+          if (paymentIntent.status === "requires_action") {
+            // Let Stripe.js handle the rest of the payment flow.
+            const { error } = await stripe.confirmCardPayment(clientSecret);
+            if (error) {
+              // The payment failed -- ask your customer for a new payment method.
+            } else {
+              // The payment has succeeded -- show a success message to your customer.
+            }
+          } else {
+            // The payment has succeeded -- show a success message to your customer.
+          }
+        }
+      });
+    }
+  }, [clientSecret, paymentRequest, stripe]);
+
+  useEffect(() => {
+    if (stripe) {
+      const pr = stripe.paymentRequest({
+        country: "US",
+        currency: "usd",
+        total: {
+          label: "Demo total",
+          amount: 1099,
+        },
+        requestPayerName: true,
+        requestPayerEmail: true,
+      });
+
+      // Check the availability of the Payment Request API.
+      pr.canMakePayment().then((result) => {
+        if (result) {
+          setPaymentRequest(pr);
+        }
+      });
+    }
+  }, [stripe]);
 
   async function _handleProceddButton(
     event: React.MouseEvent<HTMLButtonElement>
@@ -41,6 +108,9 @@ export default function CheckoutForm({
 
   return (
     <>
+      {paymentRequest && (
+        <PaymentRequestButtonElement options={{ paymentRequest }} />
+      )}
       <PaymentElement
         options={{
           fields: {
--- pages/api/create-payment-intent.ts
+++ pages/api/create-payment-intent.ts
@@ -12,24 +12,24 @@ export default async function handler(_: NextApiRequest, res: NextApiResponse) {
 
   // console.log({ customer })
 
-  const setupIntent = await stripe.setupIntents.create({
-    customer: 'cus_OGHDocHrvlsxvu', //customer.id,
-    payment_method_types: ["card"],
-    payment_method_options: {
-      us_bank_account: {
-        financial_connections: {
-          permissions: ["payment_method", "balances"],
-        },
-      },
-    },
-  });
-  // const paymentIntent = await stripe.paymentIntents.create({
-  //   amount: 1099,
-  //   currency: "USD",
-  //   payment_method_types: ['us_bank_account'],
+  // const setupIntent = await stripe.setupIntents.create({
+  //   customer: 'cus_OGHDocHrvlsxvu', //customer.id,
+  //   payment_method_types: ["card"],
+  //   payment_method_options: {
+  //     us_bank_account: {
+  //       financial_connections: {
+  //         permissions: ["payment_method", "balances"],
+  //       },
+  //     },
+  //   },
   // });
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: "USD",
+    payment_method_types: ["card"],
+  });
 
-  const { client_secret: clientSecret } = setupIntent;
+  const { client_secret: clientSecret } = paymentIntent;
   console.log({ clientSecret });
   res.status(200).json({ clientSecret });
 }
